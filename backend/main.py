08ac9258d10ecd81470a473a1a8b541c8da593ad
hongxia <w7442921@outlook.com>
2026-09-18 21:12:05 +0800

    完成analyze接口的实现

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from pypinyin import lazy_pinyin, Style
+from snownlp import SnowNLP
+
 
 
 app = FastAPI()
@@ -32,13 +35,23 @@ class AnalyticsData(BaseModel):
 def get_profile():
     return profile
 
+def score_label(score):
+    if score >= 0.6:
+        return "偏积极"
+    elif score <= 0.4:
+        return "偏消极"
+    else:
+        return "中性"
+
 @app.post("/api/analyze")
 def analyze(req: AnalyticsData):
-    return{
-        "text": req.text,
-        "score": 0.5,
-        "label":"偏平静",
-        "pinyin":"(模块六再说)",
+    text = req.text
+    score = round(SnowNLP(text).sentiments, 2)                    # 真模型打的分
+    return {
+        "text": text,
+        "score": score,
+        "label": score_label(score),                                         # ← 先留着，下面处理
+        "pinyin": " ".join(lazy_pinyin(text, style=Style.TONE)),  # 真拼音，带声调
     }
 
 
